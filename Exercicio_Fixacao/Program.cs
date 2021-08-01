49a271ba5f69cfcfe2ba7abbd4b7ce3ea47dba62
José Luiz <zecind.github@gmail.com>
2021-08-01 10:02:10 -0300

    Add project files.

---
+++ Exercicio_Fixacao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao {
+    class Program {
+        static void Main(string[] args) {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de Escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Usando Placeholders\n");
+            Console.WriteLine("Produtos:");
+            Console.WriteLine("{0}, cujo preço é $ {1:F2}\n{2}, cujo preço é $ {3:F2}", produto1, preco1, produto2, preco2);
+            Console.WriteLine("\n-----------------------------------------\n");
+            Console.WriteLine("Usando Interpolação\n");
+            Console.WriteLine($"Registro: {idade} anos de idade, código: {codigo} e gênero {genero}.");
+            Console.WriteLine("\n-----------------------------------------\n");
+            Console.WriteLine("Usando Concatenação\n");
+            Console.WriteLine("Medida com oito casas decimais: " + medida.ToString("F8"));
+            Console.WriteLine("Arredondado (três casas decimais):" + medida.ToString("F3"));
+            Console.WriteLine("Separador decimal Invariant Culture: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto {
+    class Program {
+        static void Main(string[] args) {
+
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("PLACEHOLDER\n");
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais.", nome, idade, saldo.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("\n--------------------------------------------------------------\n");
+            Console.WriteLine("INTERPOLAÇÃO\n");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo.ToString("F2", CultureInfo.InvariantCulture)} reais.");
+            Console.WriteLine("\n--------------------------------------------------------------\n");
+            Console.WriteLine("CONCATENAÇÃO - MÉTODO TRADICIONAL\n");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais.");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+            Console.WriteLine("\n--------------------------------------------------------------\n");
+        }
+    }
+}
